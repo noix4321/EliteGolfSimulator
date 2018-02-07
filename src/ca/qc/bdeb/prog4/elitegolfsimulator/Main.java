6c275c76b0c8e059607acea893edb4f2b965ca58
francis guay <gfrancis_98@hotmail.com>
2018-02-07 11:36:23 -0500

    méchant changement

--- src/ca/qc/bdeb/prog4/elitegolfsimulator/Main.java
+++ src/ca/qc/bdeb/prog4/elitegolfsimulator/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        Fenetre fenetre = new Fenetre();
+        Fenetre fenetre = new Fenetre();//trtherjstr5
         //francais yaime pas sa faire un github
     }
     
